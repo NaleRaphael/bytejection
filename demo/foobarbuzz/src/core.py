45e52b52e7930c84fbc45dfee575ef9bee19814e
NaleRaphael <gmccntwxy@gmail.com>
2019-06-25 04:12:18 +0800

    UPDATE: add a sample module for demonstration

---
+++ demo/foobarbuzz/src/core.py
@@ -0,0 +1,17 @@
+__all__ = ['foo', 'bar', 'buzz']
+
+
+def verification():
+    pass
+
+
+def foo():
+    print('foo')
+
+
+def bar():
+    print('bar')
+
+
+def buzz():
+    print('buzz')
---
+++ demo/make_pyc_dist.py
@@ -0,0 +1,43 @@
+from subprocess import Popen
+import shlex
+import shutil
+from os import makedirs
+from os import path as osp
+from pathlib import Path as pl_path
+import re
+
+
+def main():
+    package_name = 'foobarbuzz'
+    dir_target = './{}'.format(package_name)
+    cmds = [
+        'python -m compileall {}'.format(dir_target),
+    ]
+
+    for cmd in cmds:
+        proc = Popen(shlex.split(cmd))
+        proc.wait()
+
+    dir_dist = './foobarbuzz/dist'
+    if not osp.exists(dir_dist):
+        makedirs(dir_dist)
+
+    regex = re.compile('.cpython-\d{2}')
+
+    pyc_files = list(pl_path(osp.abspath(dir_target)).glob('**/*.pyc'))
+    for f in pyc_files:
+        parts = f.parts
+        idx = parts.index('__pycache__')
+        parts = parts[:idx] + parts[idx+1:]
+        idx = parts.index(package_name)
+        parts = parts[:idx+1] + ('dist',) + parts[idx+1:]
+
+        f_target = regex.sub('', osp.join(*parts))
+        print('Moving file from {} to {}'.format(f, f_target))
+        if not osp.exists(osp.dirname(f_target)):
+            makedirs(osp.dirname(f_target))
+        shutil.move(f, f_target)
+
+
+if __name__ == '__main__':
+    main()
