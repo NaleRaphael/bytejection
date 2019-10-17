d2d23db33c235ae259e2c09f565c8939658de789
NaleRaphael <gmccntwxy@gmail.com>
2019-10-17 22:04:57 +0800

    MAINT: rewrite `load_module_from_path` for better compatibility
    Module `imp` has been deprecated since version 3.3, so that we have
    to repalce those code by `importlib`.

--- demo/run_patch_module.py
+++ demo/run_patch_module.py
@@ -17,18 +17,38 @@ if not _MODULE_INSTALLED:
     del sys
 
 
-def load_module_from_path(name, path):
+def _load_module_deprecated(name, path):
     import imp
     fo, fn, info = imp.find_module(name, [path])
     return imp.load_module(name, fo, fn, info)
+
+
+def _load_module(name, path):
+    from importlib import util as im_util
+    spec = im_util.spec_from_file_location(name, path, submodule_search_locations=[])
+    if spec is not None:
+        mod = im_util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+    else:
+        mod = None
+    return mod
+
+
+def load_module_from_path(name, path):
+    from sys import version_info
+    if version_info.major < 3 or version_info.minor < 4:
+        mod = _load_module_deprecated(name, path)
+    else:
+        mod = _load_module(name, path)
+    return mod
 
 
 def main():
     try:
         import foobarbuzz.core as mod
     except ModuleNotFoundError:
-        mod = load_module_from_path('foobarbuzz', osp.join(osp.dirname(__file__), 'pkg'))
-        mod = mod.core
+        mod_path = osp.join(osp.dirname(__file__), 'pkg', 'foobarbuzz', 'core.py')
+        mod = load_module_from_path('foobarbuzz.core', mod_path)
 
     # Check the output of original function
     print('--- original version ---')
@@ -47,7 +67,7 @@ def main():
 
     # Check the output of modified function
     print('--- modified version ---')
-    modified_mod = load_module_from_path('modified', osp.dirname(__file__))
+    modified_mod = load_module_from_path('modified', osp.join(osp.dirname(__file__), 'modified.pyc'))
     modified_mod.foo()
 
 
